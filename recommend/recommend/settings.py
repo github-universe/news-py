c429c91b9808e6d57406b9f1195400a62337e125
winterfellding <winterfall.ding@gmail.com>
2018-05-24 10:33:55 +0800

    db setting

--- recommend/recommend/settings.py
+++ recommend/recommend/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'recommend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'news',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'TIME_ZONE': 'Asia/Shanghai',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
